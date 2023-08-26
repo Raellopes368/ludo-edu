46ae412b6d49010823a70ce77ea15772db421450
Raellopes368 <raellopes368@gmail.com>
2023-08-26 11:56:37 -0300

    Cria mapper de questões

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,36 @@
+import { Question } from '@app/entities/question';
+import { Groups, Questions as QuestionsRaw } from '@prisma/client';
+import { PrismaGroupMapper } from './prisma-group-mapper';
+
+interface QuestionData extends QuestionsRaw {
+  group: Groups;
+}
+
+export class PrismaQuestionMapper {
+  static toPrisma(question: Question): QuestionsRaw {
+    return {
+      content: question.content,
+      group_id: question.group_id,
+      level: question.level,
+      question_id: question.id,
+      user_id: question.user_id,
+    };
+  }
+
+  static toDomain(question: QuestionData): Question {
+    const questionDomain = new Question(
+      {
+        content: question.content,
+        group_id: question.group_id,
+        level: question.level,
+        user_id: question.user_id,
+      },
+      question.question_id,
+    );
+
+    if (question.group)
+      questionDomain.group = PrismaGroupMapper.toDomain(question.group);
+
+    return questionDomain;
+  }
+}
