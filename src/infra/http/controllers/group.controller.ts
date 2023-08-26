a338927d041779199b489a5bf0965e3a1aefb518
Raellopes368 <raellopes368@gmail.com>
2023-08-26 11:00:44 -0300

    Cria controller para grupos

---
+++ src/infra/http/controllers/group.controller.ts
@@ -0,0 +1,42 @@
+import { CreateGroups } from '@app/use-cases/user/create-groups';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateGroupBody } from '../dtos/create-group-body';
+import { GroupViewModel } from '../view-models/group-view-model';
+import { JwtAuthGuard } from '@infra/auth/jwt-auth.guard';
+import { JWTReqPayload } from 'src/interfaces';
+
+@Controller('groups')
+export class GroupController {
+  constructor(private readonly createGroup: CreateGroups) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() body: CreateGroupBody, @Req() req: JWTReqPayload) {
+    const { description, name } = body;
+    const { userId } = req.user;
+    try {
+      const { group } = await this.createGroup.execute({
+        name,
+        description,
+        teacher_owner_user_id: userId,
+      });
+
+      return {
+        group: GroupViewModel.toHTTP(group),
+      };
+    } catch (error: any) {
+      throw new HttpException(
+        'Não foi possível criar o grupo',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
