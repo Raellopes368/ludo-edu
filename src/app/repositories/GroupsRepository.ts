7d6c1aa71592211c53a3cabe9dfcfb2935d48ce0
Raellopes368 <raellopes368@gmail.com>
2023-08-26 10:21:50 -0300

    Adiciona novos métodos ao repositório de grupos

--- src/app/repositories/GroupsRepository.ts
+++ src/app/repositories/GroupsRepository.ts
@@ -2,6 +2,7 @@ import { Groups } from '@app/entities/groups';
 
 export abstract class GroupsRepository {
   abstract create(group: Groups): Promise<void>;
-  abstract getByGroupId(group_id: string): Promise<Groups>;
+  abstract getByGroupId(group_id: string): Promise<Groups | null>;
   abstract listByTeacherId(teacher_user_id: string): Promise<Groups[]>;
+  abstract findByName(name: string): Promise<Groups | null>;
 }
