2f02f5e46a24ed0f3063d95380468040c7271849
Raellopes368 <raellopes368@gmail.com>
2023-08-26 11:25:30 -0300

    Cria repositório de questões

---
+++ src/app/repositories/QuestionsRepository.ts
@@ -0,0 +1,9 @@
+import { Question } from '@app/entities/question';
+
+export abstract class QuestionsRepository {
+  abstract create(question: Question): Promise<void>;
+  abstract findById(question_id: string): Promise<Question | null>;
+  abstract listByGroup(group_id: string): Promise<Question[]>;
+  abstract listByTeacher(teacher_id: string): Promise<Question[]>;
+  abstract listByGame(group_id: string): Promise<Question[]>;
+}
