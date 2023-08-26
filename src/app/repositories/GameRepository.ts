53d3e7a9305b2d9b0cd27ec130f273d5129f4efb
Raellopes368 <raellopes368@gmail.com>
2023-08-26 14:28:49 -0300

    Cria repositório de jogos

---
+++ src/app/repositories/GameRepository.ts
@@ -0,0 +1,8 @@
+import { Game } from '@app/entities/game';
+
+export abstract class GameRepository {
+  abstract create(game: Game): Promise<void>;
+  abstract findById(game_id: string): Promise<Game | null>;
+  abstract listByTeacher(teacher_id: string): Promise<Game[]>;
+  abstract listByGroup(group_id: string): Promise<Game[]>;
+}
