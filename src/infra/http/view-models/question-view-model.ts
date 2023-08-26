9041f19fdd478d533df2d028f8e5fcb88eb493ee
Raellopes368 <raellopes368@gmail.com>
2023-08-26 12:12:44 -0300

    Cria view model de questões

---
+++ src/infra/http/view-models/question-view-model.ts
@@ -0,0 +1,15 @@
+import { Question } from '@app/entities/question';
+import { GroupViewModel } from './group-view-model';
+
+export class QuestionViewModel {
+  static toHTTP(question: Question) {
+    return {
+      content: question.content,
+      group: question.group ? GroupViewModel.toHTTP(question.group) : null,
+      group_id: question.group_id,
+      level: question.level,
+      id: question.id,
+      teacher_id: question.user_id,
+    };
+  }
+}
