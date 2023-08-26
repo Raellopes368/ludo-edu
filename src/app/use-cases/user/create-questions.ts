f6732d9edf63d01973623cbde60f2884c841c7ab
Raellopes368 <raellopes368@gmail.com>
2023-08-26 11:36:46 -0300

    Cria caso de uso para criação de questões

---
+++ src/app/use-cases/user/create-questions.ts
@@ -0,0 +1,24 @@
+import { Question } from '@app/entities/question';
+import { QuestionsRepository } from '@app/repositories/QuestionsRepository';
+import { Injectable } from '@nestjs/common';
+
+interface CreateQuestionRequest {
+  content: string;
+  level: number;
+  user_id: string;
+  group_id: string;
+}
+
+@Injectable()
+export class CreateQuestion {
+  constructor(private questionRepository: QuestionsRepository) {}
+  async execute(request: CreateQuestionRequest) {
+    const question = new Question(request);
+
+    await this.questionRepository.create(question);
+
+    return {
+      question,
+    };
+  }
+}
