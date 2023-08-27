a3a89c6c6c762d5a0d1958195861c0034d14ddd8
Raellopes368 <raellopes368@gmail.com>
2023-08-27 12:28:31 -0300

    Cria caso de uso para estudante entrar em um jogo

---
+++ src/app/use-cases/students/student-enter-the-game.ts
@@ -0,0 +1,51 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CheckUserCanJoinTheGame } from './check-user-can-join-the-game';
+import { GetPlayerPositions } from './get-player-positions';
+import { StudentPlayGameRepository } from '@app/repositories/StudentPlayGameRepository';
+import { StudentsPlayGames } from '@app/entities/studentsPlayGames';
+
+interface StudentEnterTheGameRequest {
+  player_user_id: string;
+  game_id: string;
+}
+
+interface StudentEnterTheGameResponse {
+  player: StudentsPlayGames;
+}
+
+@Injectable()
+export class StudentEnterTheGame {
+  constructor(
+    private studentPlayGameRepository: StudentPlayGameRepository,
+    private checkUserCanJoinTheGame: CheckUserCanJoinTheGame,
+    private getPlayerPositions: GetPlayerPositions,
+  ) {}
+  async execute({
+    player_user_id,
+    game_id,
+  }: StudentEnterTheGameRequest): Promise<StudentEnterTheGameResponse> {
+    const { error, game } = await this.checkUserCanJoinTheGame.execute({
+      user_id: player_user_id,
+      game_id,
+    });
+
+    if (error) throw new HttpException(error, HttpStatus.BAD_REQUEST);
+
+    const { housePosition, positionNumber } = this.getPlayerPositions.execute({
+      game,
+    });
+
+    const player = new StudentsPlayGames({
+      game_id,
+      game_position: positionNumber,
+      player_user_id,
+      start_house: housePosition.start,
+      finish_house: housePosition.end,
+    });
+
+    await this.studentPlayGameRepository.create(player);
+    return {
+      player,
+    };
+  }
+}
