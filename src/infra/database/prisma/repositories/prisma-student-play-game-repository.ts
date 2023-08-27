c02f41969a7bdb39b5ff060cc8c3b277702188e5
Raellopes368 <raellopes368@gmail.com>
2023-08-27 12:41:19 -0300

    Implementa repositório de jogadores

---
+++ src/infra/database/prisma/repositories/prisma-student-play-game-repository.ts
@@ -0,0 +1,42 @@
+import { StudentsPlayGames } from '@app/entities/studentsPlayGames';
+import { StudentPlayGameRepository } from '@app/repositories/StudentPlayGameRepository';
+import { Injectable } from '@nestjs/common';
+import { PrismaStudentPlayGameMapper } from '../mappers/prisma-student-play-game-mapper';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaStudentPlayGameRepository
+  implements StudentPlayGameRepository
+{
+  constructor(private prisma: PrismaService) {}
+  async create(studentPlayGame: StudentsPlayGames): Promise<void> {
+    const playerRaw = PrismaStudentPlayGameMapper.toPrisma(studentPlayGame);
+    await this.prisma.studentsPlayGames.create({
+      data: playerRaw,
+    });
+  }
+  async findByUserIdAndGame(
+    user_id: string,
+    game_id: string,
+  ): Promise<StudentsPlayGames> {
+    const player = await this.prisma.studentsPlayGames.findFirst({
+      where: {
+        AND: [{ player_user_id: user_id }, { game_id }],
+      },
+    });
+
+    if (!player) return null;
+    return PrismaStudentPlayGameMapper.toDomain(player);
+  }
+  async listByGame(game_id: string): Promise<StudentsPlayGames[]> {
+    const players = await this.prisma.studentsPlayGames.findMany({
+      where: {
+        game_id,
+      },
+    });
+
+    return players.map((player) =>
+      PrismaStudentPlayGameMapper.toDomain(player),
+    );
+  }
+}
