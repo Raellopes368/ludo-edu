f21d5701a53f8be9bf228272d1fb9989a5cea621
Raellopes368 <raellopes368@gmail.com>
2023-08-27 12:41:05 -0300

    Correção de nome de método

--- src/app/repositories/StudentPlayGameRepository.ts
+++ src/app/repositories/StudentPlayGameRepository.ts
@@ -2,6 +2,9 @@ import { StudentsPlayGames } from '@app/entities/studentsPlayGames';
 
 export abstract class StudentPlayGameRepository {
   abstract create(studentPlayGame: StudentsPlayGames): Promise<void>;
-  abstract findByUserId(user_id: string): Promise<StudentsPlayGames | null>;
+  abstract findByUserIdAndGame(
+    user_id: string,
+    game_id: string,
+  ): Promise<StudentsPlayGames | null>;
   abstract listByGame(game_id: string): Promise<StudentsPlayGames[]>;
 }
