007f2ac0e10c31e44f3badd77c684c03bbba09f4
Raellopes368 <raellopes368@gmail.com>
2023-08-28 17:43:17 -0300

    Adiciona método para listagem de grupos de professores

--- src/infra/http/controllers/group.controller.ts
+++ src/infra/http/controllers/group.controller.ts
@@ -2,9 +2,11 @@ import { CreateGroups } from '@app/use-cases/teacher/create-groups';
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Post,
+  Param,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -12,10 +14,15 @@ import { CreateGroupBody } from '../dtos/create-group-body';
 import { GroupViewModel } from '../view-models/group-view-model';
 import { JwtAuthGuard } from '@infra/auth/jwt-auth.guard';
 import { JWTReqPayload } from 'src/interfaces';
+import { ListGroupsParams } from '../dtos/list-groups-params';
+import { ListGroups } from '@app/use-cases/teacher/list-groups';
 
 @Controller('groups')
 export class GroupController {
-  constructor(private readonly createGroup: CreateGroups) {}
+  constructor(
+    private readonly createGroup: CreateGroups,
+    private listGroups: ListGroups,
+  ) {}
 
   @Post()
   @UseGuards(JwtAuthGuard)
@@ -35,6 +42,40 @@ export class GroupController {
     } catch (error: any) {
       throw new HttpException(
         'Não foi possível criar o grupo',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get('/list/:teacher_id')
+  async listByTeacher(@Param() { teacher_id }: ListGroupsParams) {
+    try {
+      const { groups } = await this.listGroups.execute({ user_id: teacher_id });
+
+      return {
+        groups: groups.map((group) => GroupViewModel.toHTTP(group)),
+      };
+    } catch (error: any) {
+      throw new HttpException(
+        'Não foi listar os grupos desse professor',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/list')
+  async list(@Req() req: JWTReqPayload) {
+    try {
+      const { userId: user_id } = req.user;
+      const { groups } = await this.listGroups.execute({ user_id });
+
+      return {
+        groups: groups.map((group) => GroupViewModel.toHTTP(group)),
+      };
+    } catch (error: any) {
+      throw new HttpException(
+        'Não foi listar os grupos desse professor',
         HttpStatus.BAD_REQUEST,
       );
     }
