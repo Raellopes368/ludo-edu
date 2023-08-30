c3625d57199810206c7d4148522ec306b56c7348
Raellopes368 <raellopes368@gmail.com>
2023-08-30 20:12:13 -0300

    Adiciona rota para busca de questões para usuário responder

--- src/infra/http/controllers/question.controller.ts
+++ src/infra/http/controllers/question.controller.ts
@@ -2,9 +2,11 @@ import { CreateQuestion } from '@app/use-cases/teacher/create-questions';
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Post,
+  Query,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -12,10 +14,14 @@ import { CreateQuestionBody } from '../dtos/create-question-body';
 import { QuestionViewModel } from '../view-models/question-view-model';
 import { JwtAuthGuard } from '@infra/auth/jwt-auth.guard';
 import { JWTReqPayload } from 'src/interfaces';
+import { GetQuestionToAnswer } from '@app/use-cases/students/get-question-to-answer';
 
 @Controller('questions')
 export class QuestionController {
-  constructor(private readonly createQuestions: CreateQuestion) {}
+  constructor(
+    private readonly createQuestions: CreateQuestion,
+    private readonly getQuestionToAnswer: GetQuestionToAnswer,
+  ) {}
 
   @Post()
   @UseGuards(JwtAuthGuard)
@@ -33,6 +39,30 @@ export class QuestionController {
     } catch (error: any) {
       throw new HttpException(
         'Não foi possível criar a questão',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get('/to/answer')
+  @UseGuards(JwtAuthGuard)
+  async getQuestionToPlay(
+    @Query() { game_id }: { game_id: string },
+    @Req() req: JWTReqPayload,
+  ) {
+    const { userId: user_id } = req.user;
+    try {
+      const { question } = await this.getQuestionToAnswer.execute({
+        game_id,
+        user_id,
+      });
+
+      return {
+        question: QuestionViewModel.toHTTP(question),
+      };
+    } catch (error: any) {
+      throw new HttpException(
+        error.response || 'Não foi possível buscar uma questão',
         HttpStatus.BAD_REQUEST,
       );
     }
