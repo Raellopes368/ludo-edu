804ff795a9375a92773671421d4a545627c76a64
Raellopes368 <raellopes368@gmail.com>
2023-08-30 20:09:13 -0300

    Inicia jogo passando o jogador inicial

--- src/app/use-cases/teacher/start-a-game.ts
+++ src/app/use-cases/teacher/start-a-game.ts
@@ -1,6 +1,7 @@
 import { GameRepository } from '@app/repositories/GameRepository';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CheckUserCanStartAGame } from './check-user-can-start-a-game';
+import { StudentPlayGameRepository } from '@app/repositories/StudentPlayGameRepository';
 
 interface StartAGameRequest {
   user_id: string;
@@ -12,19 +13,24 @@ export class StartAGame {
   constructor(
     private gameRepository: GameRepository,
     private checkUserCanStartAGame: CheckUserCanStartAGame,
+    private studentPlayGameRepository: StudentPlayGameRepository,
   ) {}
 
   async execute({ game_id, user_id }: StartAGameRequest) {
-    const game = await this.gameRepository.findById(game_id);
+    const [game, player] = await Promise.all([
+      this.gameRepository.findById(game_id),
+      this.studentPlayGameRepository.findFirstByGame(game_id),
+    ]);
 
     const { error } = await this.checkUserCanStartAGame.execute({
       game,
       user_id,
+      player,
     });
 
     if (error) throw new HttpException(error, HttpStatus.FORBIDDEN);
 
-    game.start();
+    game.start(player.id);
 
     await this.gameRepository.update(game);
 
