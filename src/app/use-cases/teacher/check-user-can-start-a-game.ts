ebf861da201ab45b290a5daca6aabc3d6322c71a
Raellopes368 <raellopes368@gmail.com>
2023-08-30 20:09:00 -0300

    Adiciona nova validação

--- src/app/use-cases/teacher/check-user-can-start-a-game.ts
+++ src/app/use-cases/teacher/check-user-can-start-a-game.ts
@@ -1,16 +1,18 @@
 import { Game } from '@app/entities/game';
+import { StudentsPlayGames } from '@app/entities/studentsPlayGames';
 import { Injectable } from '@nestjs/common';
 import { MinimumGameLimit } from 'src/interfaces';
 
 interface CheckUserCanStartAGameRequest {
   user_id: string;
   game: Game | null;
+  player: StudentsPlayGames;
 }
 
 @Injectable()
 export class CheckUserCanStartAGame {
-  async execute({ game, user_id }: CheckUserCanStartAGameRequest) {
-    if (!game || game.teacher_user_id !== user_id)
+  async execute({ game, user_id, player }: CheckUserCanStartAGameRequest) {
+    if (!game || game.teacher_user_id !== user_id || !player)
       return { error: 'Você não pode iniciar esse jogo' };
 
     if (!!game.winner_user_id) return { error: 'Esse jogo já foi finalizado' };
