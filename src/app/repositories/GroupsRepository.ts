8ab2983ff6feb48b52841edd68b187ee06b435f6
Raellopes368 <raellopes368@gmail.com>
2023-08-31 18:53:17 -0300

    Adiciona método para adicionar perguntas aos grupos

--- src/app/repositories/GroupsRepository.ts
+++ src/app/repositories/GroupsRepository.ts
@@ -2,6 +2,10 @@ import { Groups } from '@app/entities/groups';
 
 export abstract class GroupsRepository {
   abstract create(group: Groups): Promise<void>;
+  abstract addQuestionsToGroup(
+    group_id: string,
+    questions_id: string[],
+  ): Promise<void>;
   abstract getByGroupId(group_id: string): Promise<Groups | null>;
   abstract listByTeacherId(teacher_user_id: string): Promise<Groups[]>;
   abstract findByName(name: string): Promise<Groups | null>;
