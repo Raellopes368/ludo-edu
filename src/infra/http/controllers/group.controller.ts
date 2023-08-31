efe5e0858a562eae2d0923b10d149f910674a331
Raellopes368 <raellopes368@gmail.com>
2023-08-31 18:55:23 -0300

    Adiciona método para adicionar perguntas aos grupos

--- src/infra/http/controllers/group.controller.ts
+++ src/infra/http/controllers/group.controller.ts
@@ -16,12 +16,15 @@ import { JwtAuthGuard } from '@infra/auth/jwt-auth.guard';
 import { JWTReqPayload } from 'src/interfaces';
 import { ListGroupsParams } from '../dtos/list-groups-params';
 import { ListGroups } from '@app/use-cases/teacher/list-groups';
+import { AddQuestionsToGroupBody } from '../dtos/add-questions-to-group-body';
+import { AddQuestionsToGroup } from '@app/use-cases/teacher/add-questions-to-group';
 
 @Controller('groups')
 export class GroupController {
   constructor(
     private readonly createGroup: CreateGroups,
     private listGroups: ListGroups,
+    private addQuestionsToGroup: AddQuestionsToGroup,
   ) {}
 
   @Post()
@@ -77,6 +80,30 @@ export class GroupController {
       throw new HttpException(
         'Não foi listar os grupos desse professor',
         HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Post('/add/questions')
+  @UseGuards(JwtAuthGuard)
+  async addQuestions(
+    @Body() body: AddQuestionsToGroupBody,
+    @Req() req: JWTReqPayload,
+  ) {
+    const { group_id, questions_id } = body;
+    const { userId: user_id } = req.user;
+    try {
+      await this.addQuestionsToGroup.execute({
+        group_id,
+        questions_id,
+        user_id,
+      });
+
+      return;
+    } catch (error: any) {
+      throw new HttpException(
+        error.response || 'Não foi possível adicionar essas questões',
+        error.status || HttpStatus.BAD_REQUEST,
       );
     }
   }
