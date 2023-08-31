fb7f25a9bdc7179aff9b964769e2725f7af84902
Raellopes368 <raellopes368@gmail.com>
2023-08-31 19:38:04 -0300

    Adiciona método para listagem de questões

--- src/infra/http/controllers/question.controller.ts
+++ src/infra/http/controllers/question.controller.ts
@@ -15,12 +15,16 @@ import { QuestionViewModel } from '../view-models/question-view-model';
 import { JwtAuthGuard } from '@infra/auth/jwt-auth.guard';
 import { JWTReqPayload } from 'src/interfaces';
 import { GetQuestionToAnswer } from '@app/use-cases/students/get-question-to-answer';
+import { PaginationQuery } from '../dtos/pagination-query';
+import { ListQuestions } from '@app/use-cases/teacher/list-questions';
+import { parseToNumber } from '@helpers/parseToNumber';
 
 @Controller('questions')
 export class QuestionController {
   constructor(
     private readonly createQuestions: CreateQuestion,
     private readonly getQuestionToAnswer: GetQuestionToAnswer,
+    private readonly listQuestions: ListQuestions,
   ) {}
 
   @Post()
@@ -63,6 +67,32 @@ export class QuestionController {
     } catch (error: any) {
       throw new HttpException(
         error.response || 'Não foi possível buscar uma questão',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get('/list')
+  @UseGuards(JwtAuthGuard)
+  async list(@Query() query: PaginationQuery, @Req() req: JWTReqPayload) {
+    try {
+      const { userId: user_id } = req.user;
+      const { page, per_page } = query;
+      const { questions, total_results } = await this.listQuestions.execute({
+        user_id,
+        page: parseToNumber(page),
+        per_page: parseToNumber(per_page),
+      });
+
+      return {
+        questions: questions.map((question) =>
+          QuestionViewModel.toHTTP(question),
+        ),
+        total_results,
+      };
+    } catch (error: any) {
+      throw new HttpException(
+        'Não foi listar as questões',
         HttpStatus.BAD_REQUEST,
       );
     }
