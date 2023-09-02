4ac97eb16afd6030f85e39b6b3441bf5a27d5e2e
Raellopes368 <raellopes368@gmail.com>
2023-09-02 17:16:37 -0300

    Adiciona novo método no repositório de opções de questões

--- src/app/repositories/QuestionOptionRepository.ts
+++ src/app/repositories/QuestionOptionRepository.ts
@@ -3,4 +3,7 @@ import { QuestionOptions } from '@app/entities/question/QuestionOption';
 export abstract class QuestionOptionRepository {
   abstract createMany(questionOption: QuestionOptions[]): Promise<void>;
   abstract update(questionOption: QuestionOptions): Promise<void>;
+  abstract findById(
+    question_option_id: string,
+  ): Promise<QuestionOptions | null>;
 }
