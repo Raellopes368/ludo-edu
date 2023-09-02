48c8f86ad822f37856c50605339a3a222f393276
Raellopes368 <raellopes368@gmail.com>
2023-09-02 17:19:23 -0300

    Corrige  métodos do repositório de respostas de usuários

--- src/app/repositories/UserCheckOptionsRepository.ts
+++ src/app/repositories/UserCheckOptionsRepository.ts
@@ -3,8 +3,12 @@ import { UserCheckOptions } from '@app/entities/userCheckOptions';
 export abstract class UserCheckOptionsRepository {
   abstract create(userCheckOptions: UserCheckOptions): Promise<void>;
   abstract update(userCheckOptions: UserCheckOptions): Promise<void>;
-  abstract findByQuestion(
+  abstract findByQuestionAndGame(
     question_id: string,
+    game_id: string,
   ): Promise<UserCheckOptions | null>;
-  abstract getUserPoints(student_user_id: string): Promise<number>;
+  abstract getUserPoints(
+    student_user_id: string,
+    game_id: string,
+  ): Promise<number>;
 }
