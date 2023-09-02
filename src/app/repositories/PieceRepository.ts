e3f9e52ecc411c280fb1850341349a0e4126c3a2
Raellopes368 <raellopes368@gmail.com>
2023-09-02 18:46:11 -0300

    Correção de repositório

--- src/app/repositories/PieceRepository.ts
+++ src/app/repositories/PieceRepository.ts
@@ -1,8 +1,8 @@
 import { Piece } from '@app/entities/piece';
 
 export abstract class PieceRepository {
-  abstract create(): Promise<void>;
-  abstract update(): Promise<void>;
+  abstract create(piece: Piece): Promise<void>;
+  abstract update(piece: Piece): Promise<void>;
   abstract listByGame(game_id: string): Promise<Piece[]>;
   abstract findById(piece_id: string): Promise<Piece | null>;
 }
