9fb8c502b4e4813e624fb15dd982e65c3de23ed9
Raellopes368 <raellopes368@gmail.com>
2023-09-02 17:48:04 -0300

    Cria mapper de respostas de usuários

---
+++ src/infra/database/prisma/mappers/prisma-user-check-options-mapper.ts
@@ -0,0 +1,25 @@
+import { UserCheckOptions } from '@app/entities/userCheckOptions';
+import { UserCheckOptions as UserCheckOptionsRaw } from '@prisma/client';
+
+export class PrismaUserCheckOptionsMapper {
+  static toPrisma(userCheckOptions: UserCheckOptions): UserCheckOptionsRaw {
+    return {
+      created_at: userCheckOptions.createdAt,
+      updated_at: userCheckOptions.updatedAt,
+      question_option_id: userCheckOptions.question_option_id,
+      student_user_id: userCheckOptions.student_user_id,
+      user_check_options_id: userCheckOptions.id,
+    };
+  }
+  static toDomain(userCheckOptions: UserCheckOptionsRaw) {
+    return new UserCheckOptions(
+      {
+        created_at: userCheckOptions.created_at,
+        updated_at: userCheckOptions.updated_at,
+        question_option_id: userCheckOptions.question_option_id,
+        student_user_id: userCheckOptions.student_user_id,
+      },
+      userCheckOptions.user_check_options_id,
+    );
+  }
+}
