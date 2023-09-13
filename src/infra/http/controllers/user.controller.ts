cd91cbf181f6445fc1f190bb13e4e5290759482b
Raellopes368 <raellopes368@gmail.com>
2023-09-13 20:08:45 -0300

    Adiciona rota para buscar professores

--- src/infra/http/controllers/user.controller.ts
+++ src/infra/http/controllers/user.controller.ts
@@ -2,16 +2,23 @@ import { CreateUser } from '@app/use-cases/user/create-user';
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CreateUserBody } from '../dtos/create-user-body';
 import { UserViewModel } from '../view-models/user-view-model';
+import { SearchUsers } from '@app/use-cases/user/search-users';
+import { SearchUserQuery } from '../dtos/search-user-query';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly createUser: CreateUser) {}
+  constructor(
+    private readonly createUser: CreateUser,
+    private readonly searchUsers: SearchUsers,
+  ) {}
 
   @Post()
   async create(@Body() body: CreateUserBody) {
@@ -31,6 +38,22 @@ export class UserController {
     } catch (error: any) {
       throw new HttpException(
         'Não foi possível criar o usuário',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get('/search')
+  async search(@Query() { term = '' }: SearchUserQuery) {
+    try {
+      const { users } = await this.searchUsers.execute({ term });
+
+      return {
+        users: users.map((user) => UserViewModel.toHTTP(user)),
+      };
+    } catch (err) {
+      throw new HttpException(
+        'Houve um erro ao listar usuários',
         HttpStatus.BAD_REQUEST,
       );
     }
